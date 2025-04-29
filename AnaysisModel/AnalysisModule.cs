e602a725dfb2be2bfc66d9207274ec59c025418f
xu3545k7 <xu3545k7@gmail.com>
2025-04-29 22:40:44 +0800

    remove groq

--- AnaysisModel/AnalysisModule.cs
+++ AnaysisModel/AnalysisModule.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Drawing;
-using Groq;
 using static alglib;
 namespace AnaysisModel
 {
--- AnaysisModel/PredictionEngine.cs
+++ AnaysisModel/PredictionEngine.cs
@@ -1,5 +1,4 @@
 using static alglib;
-using Groq;
 using AnaysisModel;
 namespace AnaysisModel
 {
